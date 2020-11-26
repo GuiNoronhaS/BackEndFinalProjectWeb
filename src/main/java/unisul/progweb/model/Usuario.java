771426e4e01776d0c927450cf62b9c06da6d025f
Guilherme Noronha da Silva <guinoronha7@gmail.com>
2020-11-26 18:05:07 -0300

    primeiro commit em Dev

--- src/main/java/unisul/progweb/model/Usuario.java
+++ src/main/java/unisul/progweb/model/Usuario.java
@@ -1,8 +1,13 @@
 package unisul.progweb.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+
+@Entity
 public class Usuario {
 	
-	
+	private @Id @GeneratedValue int id;
 	private String login;
 	private String senha;
 	private String nomeCompleto;
@@ -22,7 +27,64 @@ public class Usuario {
 		this.Nascimento = Nascimento;
 		this.sexo = sexo;
 		this.estadoCivil = estadoCivil;
-		
 	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getNomeCompleto() {
+		return nomeCompleto;
+	}
+
+	public void setNomeCompleto(String nomeCompleto) {
+		this.nomeCompleto = nomeCompleto;
+	}
+
+	public int getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(int cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getNascimento() {
+		return Nascimento;
+	}
+
+	public void setNascimento(String nascimento) {
+		Nascimento = nascimento;
+	}
+
+	public String getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+
+	public String getEstadoCivil() {
+		return estadoCivil;
+	}
+
+	public void setEstadoCivil(String estadoCivil) {
+		this.estadoCivil = estadoCivil;
+	}
+	
+	
 	
 }
