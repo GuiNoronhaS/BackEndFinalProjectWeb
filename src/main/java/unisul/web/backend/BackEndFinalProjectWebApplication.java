ee4eb6db0194a35d464957352411df077e3fe220
Guilherme Noronha da Silva <guinoronha7@gmail.com>
2020-11-26 17:12:01 -0300

    Iniciando essa porra

---
+++ src/main/java/unisul/web/backend/BackEndFinalProjectWebApplication.java
@@ -0,0 +1,13 @@
+package unisul.web.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndFinalProjectWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndFinalProjectWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/unisul/web/backend/BackEndFinalProjectWebApplicationTests.java
@@ -0,0 +1,13 @@
+package unisul.web.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndFinalProjectWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
